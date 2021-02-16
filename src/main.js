757ac2563eb53b40c5db6d5b358ed6f9521350e8
Gzim <gzimasanni@gmail.com>
2021-02-16 14:45:04 +0100

    Add delete button

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ const bookName = document.getElementById('bookName');
 const bookAuthor = document.getElementById("bookAuthor");
 const pagesNumber = document.getElementById('pagesNumber')
 const statusRead = document.getElementById('status');
+const errorMessage = document.querySelector('.errormessage')
 
 function getBook(){
   let books; 
@@ -32,11 +33,13 @@ function AddBook() {
   // console.log(book)
 }
 
-// const myBooks = [{
+// const mbooks = [{
 //   name: "Game of Thrones", author: "Martin som", pages: 301, status: "finnished"
 // },
 // {name: "Game of Thrones2", author: "Martin som", pages: 301, status: "finnished"}
 // ];
+
+const mbooks = [];
 
 function Book(name, author, pages, status) {
   this.name = name;
@@ -64,22 +67,35 @@ addBook.addEventListener('click', () => {
 function booksDispaly(){
   // displayBook.innerHTML = ``;
   const dBooks = getBook();
-  dBooks.forEach((a, b) => {
+  dBooks.forEach((a, index) => {
     const card = document.createElement('div');
     card.innerHTML = `
+    <a href="#" class="delete text-danger" onclick="deleteBook(${index})" >X</a>
       ${a.name} 
       ${a.author}
       ${a.pages}
-      ${a.status}
+      ${a.status}  
     `;
     displayBook.appendChild(card);
   });
 }
 
 submit.addEventListener('click', (b) => {
-  // b.preventDefault();
-  AddBook();
-})
+  b.preventDefault();
+  if (bookName.value === '' || bookAuthor.value === '' || pagesNumber.value === '') {
+    errorMessage.classList.add('show');
+    setTimeout(() => errorMessage.classList.remove('show'),3000);
+  } else {
+    AddBook();
+  } 
+});
+
+
+function deleteBook(index) {
+  mbooks.splice(index, 1);
+  booksDispaly();
+}
+
 
 // booksDispaly();
 document.addEventListener('DOMContentLoaded', booksDispaly);
