73fbbd270cfe8bbcaef337d928118b68915653b4
Gzim <gzimasanni@gmail.com>
2021-02-16 23:31:43 +0100

    Fix linters

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const booksContainer = document.querySelector('.display-book');
 const displayBooks = document.querySelector('.display-books');
 const submit = document.querySelector('.submit');
 const bookName = document.getElementById('bookName');
-const bookAuthor = document.getElementById("bookAuthor");
-const pagesNumber = document.getElementById('pagesNumber')
+const bookAuthor = document.getElementById('bookAuthor');
+const pagesNumber = document.getElementById('pagesNumber');
 const statusRead = document.getElementById('status');
 const errorMessage = document.querySelector('.errormessage');
 const cancelForm = document.querySelector('.cancel');
@@ -21,7 +21,7 @@ function addTheBook() {
   const status = statusRead.value;
   const book = new Book(name, author, pages, status);
   mBooks.push(book);
-};
+}
 
 function Book(name, author, pages, status) {
   this.name = name;
@@ -31,39 +31,38 @@ function Book(name, author, pages, status) {
 }
 
 
-function toggleStatus(status){
-  if(status.target.classList.contains('status')){
-    if(status.target.textContent === 'Done!'){
-      status.target.textContent = 'Not yet!'
-    }else {
-      status.target.textContent = 'Done!'
+function toggleStatus(status) {
+  if (status.target.classList.contains('status')){
+    if (status.target.textContent === 'Done!'){
+      status.target.textContent = 'Not yet!';
+    } else {
+      status.target.textContent = 'Done!';
     }
   }
 }
 
 cancelForm.addEventListener('click', () => {
   mainDiv.classList.remove('show');
-  displayBooks.classList.remove('x')
+  displayBooks.classList.remove('x');
 });
 
 addBook.addEventListener('click', () => {
   mainDiv.classList.add('show');
-  displayBooks.classList.add('x')
+  displayBooks.classList.add('x');
 });
 
-function booksDispaly(){
+function booksDispaly() {
   booksContainer.innerHTML = '';
-  
   mBooks.forEach((a, index) => {
     const card = document.createElement('div');
     card.className = 'col-12 text-center';
     card.innerHTML = `<div class="card text-dark bg-white mb-3" >
-  <p class="form-control d-flex justify-content-between"><p class="bg-info px-2 py-1 text-white">${index + 1}</p><a href="#" class="delete text-danger" onclick="deleteBook(${index})" >X</a></p>
-  <p class="form-control text-white bg-info"> Book Name: ${a.name}</p>
-  <p class="form-control text-white bg-info">Author Name: ${a.author}</p>
-  <p class="form-control text-white bg-info">Number of pages: ${a.pages}</p>
-  <p class="form-control text-white bg-info"><a href="#" class="btn-warning px-2 py-1 rounded status">${a.status}</a></p>
-  </div>`;
+        <p class="form-control d-flex justify-content-between"><p class="bg-info px-2 py-1 text-white">${index + 1}</p><a href="#" class="delete text-danger" onclick="deleteBook(${index})" >X</a></p>
+        <p class="form-control text-white bg-info"> Book Name: ${a.name}</p>
+        <p class="form-control text-white bg-info">Author Name: ${a.author}</p>
+        <p class="form-control text-white bg-info">Number of pages: ${a.pages}</p>
+        <p class="form-control text-white bg-info"><a href="#" class="btn-warning px-2 py-1 rounded status">${a.status}</a></p>
+      </div>`;
   booksContainer.appendChild(card);
   });
 }
@@ -75,23 +74,26 @@ function clearfield() {
 
 window.addEventListener('DOMContentLoaded', booksDispaly);
 
-booksContainer.addEventListener('click', toggleStatus)
+booksContainer.addEventListener('click', toggleStatus);
 
 submit.addEventListener('click', (b) => {
   b.preventDefault();
   if (bookName.value === '' || bookAuthor.value === '' || pagesNumber.value === '') {
     errorMessage.classList.add('show');
-    setTimeout(() => errorMessage.classList.remove('show'),3000);
+    setTimeout(() => errorMessage.classList.remove('show'), 3000);
   } else {
     addTheBook();
     clearfield();
     booksDispaly();
     mainDiv.classList.remove('show');
-    displayBooks.classList.remove('x')
+    displayBooks.classList.remove('x');
   } 
 });
+
+/* eslint-disable no-unused-vars */
 
 function deleteBook(index) {
   mBooks.splice(index, 1);
   booksDispaly();
 }
+/* eslint-enable no-unused-vars */
